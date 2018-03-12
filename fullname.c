c491d2a8e0bb2f3c51903c53a4ac281104aeb519
Angelo Caravella <sannita6570@gmailcom>
2018-03-12 19:02:59 +0100

    Stampa il nome completo

---
+++ fullname.c
@@ -0,0 +1,12 @@
+#programma che stampa il tuo nome completo utilizzando due chiamate a printf()
+#fullname.c
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Angelo ");
+	printf("Caravella\n");
+
+	return 0;
+}
