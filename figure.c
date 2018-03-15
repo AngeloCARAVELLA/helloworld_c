61fba9467e196623d95854240991b196a8b093a0
Angelo Caravella <sannita6570@gmailcom>
2018-03-15 14:02:26 +0100

    figure

---
+++ figure.c
@@ -0,0 +1,19 @@
+/*figure*/ 
+/*programma che stampa delle figure*/
+ 
+
+
+
+#include <stdio.h>
+
+int main()
+{
+	printf(" ********      ****\n"
+               " *      *      *  *      ***\n"
+	       " *      *      *  *      * *\n"
+	       " *      *      *  *      * *\n"
+	       " *      *      *  *      * *\n"
+	       " ********      ****      ***\n");
+
+	return 0;
+}
