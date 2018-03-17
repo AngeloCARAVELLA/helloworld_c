886b8750420aa97e4a072f19d011b8441aae8e60
Angelo Caravella <sannita6570@gmailcom>
2018-03-17 13:00:34 +0100

    stampa circonferenza e superficie di un cerchio dato il raggio (intero)

--- cerchio.c
+++ cerchio.c
@@ -1,22 +1,28 @@
-/*cerchio.c*/
+/* cerchio.c */
 /* programma che stampa circonferenza e superficie di un cerchio dato il raggio */
 
 #include <stdio.h>
 #include <math.h>
-int main()
 
-{/* dichiarazione delle variabili */      
-       	int r , c , s ;
-/*lettura in input del raggio */ 
+int main()
+{ 
+	/* dichiarazione delle variabili */      
+       	int r ;
+	double c , s ; 
+
+	/*lettura in input del raggio */ 
 	printf (" inserisci il raggio ");
-	scanf ( "%d" , &r);
-/* calcoli*/
-		c = M_PI * r * r ; 
-                s = M_PI * 2 * r ;
-		/* stampa */
-		printf (" la circonferenza e' %d " , c );
-			 printf (" la superficie e' ' %d " , s);
-return 0 ;
+	scanf ("%d", &r);
+
+	/* calcoli*/
+	s = M_PI * r * r ; 
+	c = M_PI * 2.0 * r ;
+
+	/* stampa */
+	printf (" la circonferenza e' %f\n" , c );
+	printf (" la superficie e' ' %f " , s);
+
+	return 0 ;
 }	
 
 
