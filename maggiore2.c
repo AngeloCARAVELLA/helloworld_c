d44e70db7fb3f2952a2fa6a5f405bed861dd10c4
Angelo Caravella <sannita6570@gmailcom>
2018-03-19 12:38:07 +0100

    maggiore tra due numeri

--- maggiore2.c
+++ maggiore2.c
@@ -1,31 +1,31 @@
 /* maggiore2.c */
 /* chiede due interi all'utente e stampa il maggiore tra i due */
+
 #include <stdio.h>
+
 int main ()
 { 
-	/* dichiarazione di due numeri interi */
-	int a , b ;
-	/* lettura in input dei due numeri interi */ 
-	/*  definizione della variabile M che rappresenta il massimo tra i due */
-	int M ;
-	/* stampa del primo messaggio */
-	printf (" inserisci il primo numero ");
-	/* input da tastiera del primo numero */ 
-	scanf ( "%d", &a );
+  /* dichiarazione di due numeri interi */
+  int a, b;
 
-	/* stampa del secondo  messaggio */
-	printf (" inserisci il secondo  numero ");
-	/* input da tastiera del secondo numero */
-	scanf ( "%d", &b );
-	/* controllo del maggiore  */
-	       M = a ;	
-	if ( M  < b ) {
-	M = b ; 
-	printf("Il maggiore e' %d\n", M);
-	}
-	/* caso in cui a é maggiore di b , rappresentato dall' else */
-	else 
-	{
-	printf("Il maggiore e' %d\n", M);
-	}
-        }
+  /* M rappresenta il massimo tra i due */
+  int M;
+
+  /* lettura in input dei due numeri interi */ 
+  printf("inserisci il primo numero: ");
+  /* input da tastiera del primo numero */ 
+  scanf("%d", &a);
+  printf("inserisci il secondo numero: ");
+  /* input da tastiera del secondo numero */
+  scanf("%d", &b);
+
+  /* controllo del maggiore  */
+  M = a ;	
+  if ( M  < b ) {
+    M = b; 
+  }
+
+  printf("Il maggiore e' %d\n", M);
+
+  return 0;
+}
