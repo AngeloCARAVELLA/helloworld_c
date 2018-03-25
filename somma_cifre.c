3e1fef4916d6a767644a8b4111ebb613e2da1d84
Angelo Caravella <sannita6570@gmailcom>
2018-03-25 18:20:27 +0200

    somma delle cifre di un numero intero a 3 cifre

---
+++ somma_cifre.c
@@ -0,0 +1,30 @@
+/* Programma che che chiede all' utente numero di tre cifre */ 
+/* E che stampa la somma delle cifre stesse */
+#include <stdio.h>
+int main (){
+  /* dichiarazione di un numero intero a 3 cifre */
+  int n ; 
+  /* dichiarazione delle cifre prima estratte e poi sommate */
+  int  c1 , c2 , c3 ;
+  /* dichiarazione della variabile somma */
+  int somma ;
+  /* messaggio in output e lettura del numero */
+  printf("inserisci un numero di tre cifre");
+  scanf("%d", &n );
+  /* condizione */ 
+  if ( n < 0 && n > 999){
+printf("non ha inserito un numero di tre cifre.bye/n");
+  }
+  else{
+  /* estraiamo le cifre */ 
+  c3 = n % 10;
+  n = n / 10;
+  c2 = n % 10;
+  n = n / 10;
+  c1 = n % 10;
+  somma = c1 + c2 + c3;
+  /* output */
+  printf("La somma delle cifre e' %d\n", somma);
+                            }
+  return 0;
+}
