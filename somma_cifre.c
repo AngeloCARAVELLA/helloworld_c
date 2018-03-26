dd410798352de29a9f856332e66bc6f7f4bae4d9
Angelo Caravella <sannita6570@gmailcom>
2018-03-26 12:11:04 +0200

    chiede all'utente un numero di tre cifre e stampa la somma delle cifre stesse

--- somma_cifre.c
+++ somma_cifre.c
@@ -1,21 +1,26 @@
 /* Programma che che chiede all' utente numero di tre cifre */ 
 /* E che stampa la somma delle cifre stesse */
 #include <stdio.h>
-int main (){
+int main ()
+{
   /* dichiarazione di un numero intero a 3 cifre */
   int n ; 
   /* dichiarazione delle cifre prima estratte e poi sommate */
   int  c1 , c2 , c3 ;
   /* dichiarazione della variabile somma */
   int somma ;
+
   /* messaggio in output e lettura del numero */
   printf("inserisci un numero di tre cifre");
   scanf("%d", &n );
+
   /* condizione */ 
-  if ( n < 0 && n > 999){
-printf("non ha inserito un numero di tre cifre.bye/n");
+  if (n < 0 || n > 999) 
+  {
+    printf("non ha inserito un numero di tre cifre.bye/n");
   }
-  else{
+  else
+  {
   /* estraiamo le cifre */ 
   c3 = n % 10;
   n = n / 10;
@@ -24,7 +29,7 @@ printf("non ha inserito un numero di tre cifre.bye/n");
   c1 = n % 10;
   somma = c1 + c2 + c3;
   /* output */
-  printf("La somma delle cifre e' %d\n", somma);
-                            }
+  printf("La somma delle cifre e' %d\n", somma);  
+  }
   return 0;
 }
